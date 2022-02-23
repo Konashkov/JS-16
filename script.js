4d4d9770089d51a4ca8da2dfda294dcd1578295b
Olexiy Konashkov <konashkovolexiy@gmail.com>
2022-02-23 17:34:40 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let me = {
+    name: "Alexey",
+    age: 14,
+    lang:['JS','HTML','CSS','C#','C++'],
+    address:{
+        street: 'Khorolska',
+        building: '1a',
+        flat: 5,
+    },
+    
+}
+function output(data){
+    console.log(data)
+}
+output(me)
